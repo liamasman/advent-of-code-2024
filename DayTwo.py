f62fe8958aecd2fd952b049d5da9338a9848e1c7
Liam Asman <liam@liamasman.com>
2024-12-02 20:13:54 +0000

    2024 Day Two

---
+++ DayTwo.py
@@ -0,0 +1,63 @@
+from itertools import pairwise
+
+def is_safe_part_one(input):
+    input = convert_lines(input)
+    safe = 0
+    for line in input:
+        if is_line_safe_part_one(line):
+            safe += 1
+    return safe
+
+def convert_lines(input):
+    lines = []
+    for line in input:
+        lines.append([int(i) for i in line.split(" ")])
+    return lines
+
+def is_line_safe_part_one(line):
+    if len(line) < 2:
+        return True
+    return is_gradually_ascending(line) or is_gradually_descending(line)
+
+def is_gradually_ascending(line):
+    for a, b in pairwise(line):
+        if a >= b:
+            return False
+        if b - a > 3:
+            return False
+    return True
+
+def is_gradually_descending(line):
+    return is_gradually_ascending(line[::-1])
+
+def is_safe_part_two(input):
+    input = convert_lines(input)
+    safe = 0
+    for line in input:
+        if is_line_safe_part_two(line):
+            safe += 1
+    return safe
+
+def is_line_safe_part_two(line):
+    if len(line) <= 2:
+        return True
+
+    if is_line_safe_part_one(line):
+        return True
+
+    for i, level in enumerate(line):
+        line_copy = line.copy()
+        del line_copy[i]
+        if is_line_safe_part_one(line_copy):
+            return True
+    return False
+
+if "__main__" == __name__:
+    input = open("resources/Day2Input.txt", "r")
+    lines = input.readlines()
+
+    safe1 = is_safe_part_one(lines)
+    print(f"Part One, Safe: {safe1}")
+
+    safe2 = is_safe_part_two(lines)
+    print(f"Part Two, Safe: {safe2}")
---
+++ DayTwoTest.py
@@ -0,0 +1,150 @@
+import unittest
+
+from DayTwo import is_safe_part_one, is_safe_part_two
+
+
+class PartOne(unittest.TestCase):
+    def test_given_case(self):
+        input = ["7 6 4 2 1",
+                 "9 7 6 2 1",
+                 "1 3 2 4 5",
+                 "8 6 4 4 1",
+                 "1 3 6 7 9"]
+        self.assertEqual(2, is_safe_part_one(input))
+
+    def test_single_line_ascending(self):
+        input = ["1 2 3 4 5"]
+        self.assertEqual(1, is_safe_part_one(input))
+
+    def test_single_line_descending(self):
+        input = ["5 4 3 2 1"]
+        self.assertEqual(1, is_safe_part_one(input))
+
+    def test_single_line_ascending_with_duplicate_number(self):
+        input = ["1 1 2 3 4"]
+        self.assertEqual(0, is_safe_part_one(input))
+
+    def test_single_line_descending_with_duplicate_number(self):
+        input = ["5 4 3 2 2"]
+        self.assertEqual(0, is_safe_part_one(input))
+
+    def test_single_line_ascending_quickly(self):
+        input = ["1 2 8 9 10"]
+        self.assertEqual(0, is_safe_part_one(input))
+
+    def test_single_line_descending_quickly(self):
+        input = ["10 9 3 2 1"]
+        self.assertEqual(0, is_safe_part_one(input))
+
+    def test_single_line_up_and_down(self):
+        input = ["1 2 3 2 1"]
+        self.assertEqual(0, is_safe_part_one(input))
+
+class PartTwo(unittest.TestCase):
+    def test_given_case(self):
+        input = ["7 6 4 2 1",
+                 "9 7 6 2 1",
+                 "1 3 2 4 5",
+                 "8 6 4 4 1",
+                 "1 3 6 7 9"]
+        self.assertEqual(4, is_safe_part_two(input))
+
+    def test_single_line_ascending(self):
+        input = ["1 2 3 4 5"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_descending(self):
+        input = ["5 4 3 2 1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_one_duplicate_number(self):
+        input = ["1 1 2 3 4"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_descending_with_one_duplicate_number(self):
+        input = ["5 4 3 2 2"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_quickly(self):
+        input = ["1 2 8 9 10"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_descending_quickly(self):
+        input = ["10 9 3 2 1"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_up_and_down(self):
+        input = ["1 2 3 2 1"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_up_and_down_with_duplicate_number(self):
+        input = ["1 2 3 2 2"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_duplicate_number_at_end(self):
+        input = ["1 2 3 4 4"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_descending_with_duplicate_number_at_beginning(self):
+        input = ["4 4 3 2 1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_up_and_down_with_duplicate_number_at_end_and_beginning(self):
+        input = ["4 4 3 2 4"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_big_jump_at_final_number(self):
+        input = ["1 2 3 4 8"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_descending_with_big_jump_at_final_number(self):
+        input = ["8 7 6 5 1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_irregular_middle_number(self):
+        input = ["1 2 5 4 5"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_descending_with_small_start(self):
+        input = ["6 7 6 5 4"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_large_start(self):
+        input = ["5 4 5 6 7"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_with_two_duplicates(self):
+        input = ["1 1 2 2 3"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_with_three_duplicates(self):
+        input = ["1 1 1 2 2"]
+        self.assertEqual(0, is_safe_part_two(input))
+
+    def test_single_line_with_one_item(self):
+        input = ["1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_with_two_items_ascending(self):
+        input = ["1 2"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_with_two_items_descending(self):
+        input = ["2 1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_with_two_items_same(self):
+        input = ["1 1"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_irregular_second_number(self):
+        input = ["1 4 2 4 5"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+    def test_single_line_ascending_with_irregular_first_number(self):
+        input = ["1 6 7 8 9"]
+        self.assertEqual(1, is_safe_part_two(input))
+
+
+if __name__ == '__main__':
+    unittest.main()
