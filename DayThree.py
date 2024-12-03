abc5c0efa0b6df72f18e0d48b459d8d490cb558c
Liam Asman <liam@liamasman.com>
2024-12-03 19:57:33 +0000

    Day Three Part Two

--- DayThree.py
+++ DayThree.py
@@ -7,9 +7,23 @@ def dayThreePartOne(input):
         result += int(match.group(1)) * int(match.group(2))
     return result
 
+def dayThreePartTwo(input):
+    pattern = re.compile(r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))")
+    mul_enabled = True
+    result = 0
+    for match in pattern.finditer(input):
+        if match.group(1) == "do()":
+            mul_enabled = True
+        elif match.group(1) == "don't()":
+            mul_enabled = False
+        elif mul_enabled:
+            result += int(match.group(2)) * int(match.group(3))
+    return result
+
 if __name__ == "__main__":
     input = open("resources/Day3Input.txt", "r")
     input = input.read()
 
-    result = dayThreePartOne(input)
-    print(f"Part One: {result}")
+    partOneResult = dayThreePartOne(input)
+    partTwoResult = dayThreePartTwo(input)
+    print(f"Part One: {partOneResult}\nPart Two: {partTwoResult}")
--- DayThreeTest.py
+++ DayThreeTest.py
@@ -1,48 +1,69 @@
 import unittest
 
-from DayThree import dayThree
+from DayThree import dayThreePartOne
+from DayThree import dayThreePartTwo
 
-
-class DayThreePartOne(unittest.TestCase):
+class DayThreePartOneTest(unittest.TestCase):
     def test_given_test_case(self):
         input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-        self.assertEqual(161, dayThree(input))
+        self.assertEqual(161, dayThreePartOne(input))
 
     def test_single_mul_single_digits(self):
         input = "mul(3,3)"
-        self.assertEqual(9, dayThree(input))
+        self.assertEqual(9, dayThreePartOne(input))
 
     def test_single_mul_two_digits(self):
         input = "mul(44,33)"
-        self.assertEqual(1452, dayThree(input))
+        self.assertEqual(1452, dayThreePartOne(input))
 
     def test_single_mul_three_digits(self):
         input = "mul(444,333)"
-        self.assertEqual(147852, dayThree(input))
+        self.assertEqual(147852, dayThreePartOne(input))
 
     def test_single_line_no_mul(self):
         input = "mu38ry"
-        self.assertEqual(0, dayThree(input))
+        self.assertEqual(0, dayThreePartOne(input))
 
     def test_two_muls(self):
         input = "mul(3,3)mul(3,3)"
-        self.assertEqual(18, dayThree(input))
+        self.assertEqual(18, dayThreePartOne(input))
 
     def test_multi_line(self):
         input = "mul(3,3)mul(3,3)\nmul(3,3)"
-        self.assertEqual(27, dayThree(input))
+        self.assertEqual(27, dayThreePartOne(input))
 
     def test_large_numbers_not_valid(self):
         input = "mul(2441,3)"
-        self.assertEqual(0, dayThree(input))
+        self.assertEqual(0, dayThreePartOne(input))
 
     def test_missing_numbers_not_valid(self):
         input = "mul(3,)"
-        self.assertEqual(0, dayThree(input))
+        self.assertEqual(0, dayThreePartOne(input))
 
     def test_spaces_not_valid(self):
         input = "mul(4, 5)"
-        self.assertEqual(0, dayThree(input))
+        self.assertEqual(0, dayThreePartOne(input))
+
+class DayThreePartTwoTest(unittest.TestCase):
+    def test_given_testcase(self):
+        input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        self.assertEqual(48, dayThreePartTwo(input))
+
+    def test_no_instructions(self):
+        input = "mul(2,4)"
+        self.assertEqual(8, dayThreePartTwo(input))
+
+    def test_start_with_do(self):
+        input = "do()mul(2,4)"
+        self.assertEqual(8, dayThreePartTwo(input))
+
+    def test_start_with_dont(self):
+        input = "don't()mul(2,4)"
+        self.assertEqual(0, dayThreePartTwo(input))
+
+    def test_dont_do_mul(self):
+        input = "don't()do()mul(2,4)"
+        self.assertEqual(8, dayThreePartTwo(input))
 
 
 if __name__ == '__main__':
