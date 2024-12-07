5127d435333fbe7dd6f4ef48ee50195f88175c26
Liam Asman <liam@liamasman.com>
2024-12-07 13:04:51 +0000

    Day 7 Part Two

--- src/day_seven.py
+++ src/day_seven.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+
 def parse_input(raw_input: str) -> list[tuple[int, list[int]]]:
     equations = []
     for line in raw_input.split('\n'):
@@ -7,24 +10,62 @@ def parse_input(raw_input: str) -> list[tuple[int, list[int]]]:
     return equations
 
 
-def can_satisfy_equation(target: int, operands: list[int]) -> bool:
+def can_satisfy_equation_part_one(target: int, operands: list[int]) -> bool:
     if len(operands) == 1:
         return target == operands[0]
-    if target % operands[-1] == 0 and can_satisfy_equation(target //
-                                                           operands[-1],
-                                                           operands[:-1]):
+    if target % operands[-1] == 0 and can_satisfy_equation_part_one(target //
+                                                                    operands[
+                                                                        -1],
+                                                                    operands[
+                                                                    :-1]):
         return True
-    if target > operands[-1] and can_satisfy_equation(target -
-                                                           operands[-1],
-                                                           operands[:-1]):
+    if target > operands[-1] and can_satisfy_equation_part_one(target -
+                                                               operands[-1],
+                                                               operands[:-1]):
         return True
     return False
+
+
+def concatenate_operands(param1: int, param2: int) -> int:
+    return int(str(param1) + str(param2))
+
+
+def multiply_operands(param1: int, param2: int) -> int:
+    return param1 * param2
+
+
+def sum_operands(param1: int, param2: int) -> int:
+    return param1 + param2
+
+
+def modify_params(operands: list[int], operator: Callable[[int, int],
+int]) -> list[int]:
+    new_value: int = operator(operands[0], operands[1])
+    return [new_value] + operands[2:]
+
+
+def can_satisfy_equation_part_two(target: int, operands: list[int]) -> bool:
+    if len(operands) == 1:
+        return target == operands[0]
+    return can_satisfy_equation_part_two(target, modify_params(operands,
+                                                               concatenate_operands)) or \
+        can_satisfy_equation_part_two(target, modify_params(operands,
+                                                            multiply_operands)) or \
+        can_satisfy_equation_part_two(target, modify_params(operands,
+                                                            sum_operands))
 
 
 def part_one(raw_input: str) -> int:
     inputs = parse_input(raw_input)
     result = sum([target for target, operands in inputs if
-                  can_satisfy_equation(target, operands)])
+                  can_satisfy_equation_part_one(target, operands)])
+    return result
+
+
+def part_two(raw_input: str) -> int:
+    inputs = parse_input(raw_input)
+    result = sum([target for target, operands in inputs if
+                  can_satisfy_equation_part_two(target, operands)])
     return result
 
 
@@ -32,3 +73,4 @@ if __name__ == '__main__':
     with open('../resources/Day7Input.txt') as f:
         raw_input = f.read()
     print(f"part one: {part_one(raw_input)}")
+    print(f"part two: {part_two(raw_input)}")
--- tests/test_day_seven.py
+++ tests/test_day_seven.py
@@ -1,4 +1,5 @@
-from day_seven import parse_input, can_satisfy_equation, part_one
+from day_seven import parse_input, can_satisfy_equation_part_one, part_one, \
+    part_two, can_satisfy_equation_part_two
 
 
 class TestParseInput:
@@ -18,26 +19,42 @@ class TestParseInput:
         assert parse_input(raw_input) == [(1, [2, 3]), (1, [4, 5])]
 
 
-class TestCanSatisfyEquation:
+class TestCanSatisfyEquationPartOne:
     def test_can_satisfy_two_operands_with_multiplication(self):
-        assert can_satisfy_equation(6, [2, 3])
+        assert can_satisfy_equation_part_one(6, [2, 3])
 
     def test_can_satisfy_two_operands_with_addition(self):
-        assert can_satisfy_equation(5, [2, 3])
+        assert can_satisfy_equation_part_one(5, [2, 3])
 
     def test_cannot_satisfy_two_operands(self):
-        assert not can_satisfy_equation(5, [2, 2])
+        assert not can_satisfy_equation_part_one(5, [2, 2])
 
     def test_satisfying_given_cases(self):
-        assert can_satisfy_equation(190, [10, 19])
-        assert can_satisfy_equation(3267, [81, 40, 27])
-        assert can_satisfy_equation(292, [11, 6, 16, 20])
-        assert not can_satisfy_equation(83, [17, 5])
-        assert not can_satisfy_equation(156, [15, 6])
-        assert not can_satisfy_equation(7290, [6, 8, 6, 15])
-        assert not can_satisfy_equation(161011, [16, 10, 13])
-        assert not can_satisfy_equation(192, [17, 8, 14])
-        assert not can_satisfy_equation(21037, [9, 7, 18, 13])
+        assert can_satisfy_equation_part_one(190, [10, 19])
+        assert can_satisfy_equation_part_one(3267, [81, 40, 27])
+        assert can_satisfy_equation_part_one(292, [11, 6, 16, 20])
+        assert not can_satisfy_equation_part_one(83, [17, 5])
+        assert not can_satisfy_equation_part_one(156, [15, 6])
+        assert not can_satisfy_equation_part_one(7290, [6, 8, 6, 15])
+        assert not can_satisfy_equation_part_one(161011, [16, 10, 13])
+        assert not can_satisfy_equation_part_one(192, [17, 8, 14])
+        assert not can_satisfy_equation_part_one(21037, [9, 7, 18, 13])
+
+class TestCanSatisfyEquationPartTwo:
+    def test_given_example(self):
+        assert can_satisfy_equation_part_two(156, [15, 6])
+        assert can_satisfy_equation_part_two(7290, [6, 8, 6, 15])
+        assert can_satisfy_equation_part_two(192, [17, 8, 14])
+
+    def test_original_equations_can_be_satisfied(self):
+        assert can_satisfy_equation_part_two(190, [10, 19])
+        assert can_satisfy_equation_part_two(3267, [81, 40, 27])
+        assert can_satisfy_equation_part_two(292, [11, 6, 16, 20])
+
+    def test_unsatisfiable_equantions(self):
+        assert not can_satisfy_equation_part_two(83, [17, 5])
+        assert not can_satisfy_equation_part_two(161011, [16, 10, 13])
+        assert not can_satisfy_equation_part_two(21037, [9, 7, 18, 13])
 
 def test_part_one_given_case():
     raw_input = """190: 10 19
@@ -50,3 +67,15 @@ def test_part_one_given_case():
 21037: 9 7 18 13
 292: 11 6 16 20"""
     assert part_one(raw_input) == 3749
+
+def test_part_two_given_case():
+    raw_input = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+    assert part_two(raw_input) == 11387
