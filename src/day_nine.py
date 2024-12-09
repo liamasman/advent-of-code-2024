6593cf519f3e96ec01aac523bd29e2b39dd7d09f
Liam Asman <liam@liamasman.com>
2024-12-09 14:09:46 +0000

    Day 9 Part Two

--- src/day_nine.py
+++ src/day_nine.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Optional
 
 
 def create_memory_view_from_disk_map(raw_input: str) -> list:
@@ -18,7 +18,7 @@ def create_memory_view_from_disk_map(raw_input: str) -> list:
     return result
 
 
-def generate_defragged_memory_block(memory_view: list[int]) -> Generator[int,
+def generate_defragged_memory_block_part_one(memory_view: list[int]) -> Generator[int,
 None, None]:
     forward_pointer = 0
     backward_pointer = len(memory_view) - 1
@@ -34,21 +34,72 @@ None, None]:
             backward_pointer -= 1
 
 
-def calculate_checksum(memory_view: list[int]) -> int:
-    return sum([index * value for index, value in enumerate(memory_view)])
+def has_enough_space_for_size(memory_view, size_of_block, start_index):
+    for i in range(size_of_block):
+        if memory_view[start_index + i] is not None:
+            return False
+    return True
 
+
+def create_defragged_memory_block_part_two(memory_view: list[Optional[int]]):
+    last_seen_index_with_space_for_size_i: list[int] = [0] * 10
+    back_pointer = len(memory_view) - 1
+    while back_pointer >= min(last_seen_index_with_space_for_size_i):
+        if memory_view[back_pointer] is None:
+            back_pointer -= 1
+            continue
+
+        end_of_block = back_pointer
+        while back_pointer >= 0 and memory_view[back_pointer] == memory_view[\
+                end_of_block]:
+            back_pointer -= 1
+        start_of_block = back_pointer + 1
+
+        size_of_block = end_of_block - start_of_block + 1
+
+        if (last_seen_index_with_space_for_size_i[size_of_block] >=
+                start_of_block):
+            continue
+
+        found_space = False
+        # Find next large-enough empty space
+        while last_seen_index_with_space_for_size_i[size_of_block] < start_of_block:
+            if not has_enough_space_for_size(memory_view, size_of_block,
+                                         last_seen_index_with_space_for_size_i[size_of_block]):
+                last_seen_index_with_space_for_size_i[size_of_block] += 1
+            else:
+                found_space = True
+                break
+
+        if found_space:
+            # Move block to empty space
+            for i in range(size_of_block):
+                memory_view[last_seen_index_with_space_for_size_i[size_of_block] + i] = memory_view[start_of_block + i]
+                memory_view[start_of_block + i] = None
+            last_seen_index_with_space_for_size_i[size_of_block] += size_of_block
+
+
+def calculate_checksum(memory_view: list[int]) -> int:
+    return sum([index * value for index, value in enumerate(memory_view) if value is not None])
 
 
 def part_one(raw_input: str) -> int:
     memory_view = create_memory_view_from_disk_map(raw_input)
-    defragged = list(generate_defragged_memory_block(memory_view))
+    defragged = list(generate_defragged_memory_block_part_one(memory_view))
     return calculate_checksum(defragged)
+
+
+def part_two(raw_input: str) -> int:
+    memoryview = create_memory_view_from_disk_map(raw_input)
+    create_defragged_memory_block_part_two(memoryview)
+    return calculate_checksum(memoryview)
 
 
 def main():
     with open("../resources/Day9Input.txt") as f:
         raw_input = f.read()
     print(f"Part One: {part_one(raw_input)}")
+    print(f"Part Two: {part_two(raw_input)}")
 
 
 if __name__ == "__main__":
--- tests/test_day_nine.py
+++ tests/test_day_nine.py
@@ -1,5 +1,6 @@
 from day_nine import part_one, create_memory_view_from_disk_map, \
-    generate_defragged_memory_block, calculate_checksum
+    generate_defragged_memory_block_part_one, calculate_checksum, \
+    create_defragged_memory_block_part_two, part_two
 
 
 class TestCreateMemoryViewFromDiskMap:
@@ -10,37 +11,73 @@ class TestCreateMemoryViewFromDiskMap:
         assert create_memory_view_from_disk_map("2") == [0, 0]
 
     def test_multiple_files_no_gaps(self):
-        assert create_memory_view_from_disk_map("10407") == [0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
+        assert create_memory_view_from_disk_map("10407") == [0, 1, 1, 1, 1, 2,
+                                                             2, 2, 2, 2, 2, 2]
 
     def test_multiple_files_with_gaps(self):
-        assert create_memory_view_from_disk_map("42313") == [0, 0, 0, 0, None, None, 1, 1, 1,
+        assert create_memory_view_from_disk_map("42313") == [0, 0, 0, 0, None,
+                                                             None, 1, 1, 1,
                                                              None, 2, 2, 2]
 
 
-class TestGenerateDefraggedMemoryBlock:
+class TestGenerateDefraggedMemoryBlockPartOne:
     def test_defrag(self):
         memory_view = [0, 0, 0, 0, None, None, 1, 1, 1, None, 2, 2, 2]
-        assert list(generate_defragged_memory_block(memory_view)) == [0, 0,
-                                                                      0, 0,
-                                                                      2, 2,
-                                                                      1, 1,
-                                                                      1, 2]
+        assert list(generate_defragged_memory_block_part_one(memory_view)) == [
+            0, 0,
+            0, 0,
+            2, 2,
+            1, 1,
+            1, 2]
 
     def test_defrag_requiring_multiple_bloacks_from_end_to_fill(self):
         memory_view = [0, None, None, None, None, 1, 1, None, 2, None, 3,
                        None, 4, 4]
-        assert list(generate_defragged_memory_block(memory_view)) == [0, 4,
-                                                                      4, 3,
-                                                                      2, 1, 1]
+        assert list(generate_defragged_memory_block_part_one(memory_view)) == [
+            0, 4,
+            4, 3,
+            2, 1, 1]
+
+
+class TestGenerateDefraggedMemoryBlockPartTwo:
+    def test_defrag_that_makes_no_changes(self):
+        memory_view = [0, 0, 0, 0, None, None, 1, 1, 1, None, 2, 2, 2]
+        expected = [0, 0, 0, 0, None, None, 1, 1, 1, None, 2, 2, 2]
+        create_defragged_memory_block_part_two(memory_view)
+        assert memory_view == expected
+
+    def test_defrag_that_results_in_fully_condensed(self):
+        memory_view = [0, None, None, None, None, 1, 1, None, 2, None, 3,
+                       None, 4, 4]
+        create_defragged_memory_block_part_two(memory_view)
+        assert memory_view == [0, 4, 4, 3, 2, 1, 1, None, None, None, None,
+                               None, None, None]
+
+    def test_defrag_that_requires_skipping_blocks(self):
+        memory_view = [0, None, None, None, 1, 1, None, None, 2, 2, 2, None,
+                       None, None, None, None, 3, 3, 3, 3, 3, None, 4, None,
+                       5, None, 6, 6]
+        create_defragged_memory_block_part_two(memory_view)
+        assert memory_view == [0, 6, 6, 5, 1, 1, 4, None, 2, 2, 2, 3, 3, 3, 3,
+                               3, None, None, None, None, None, None, None,
+                               None, None, None, None, None]
 
 
 class TestCalculateChecksum:
     def test_calculate_checksum(self):
         assert calculate_checksum([0, 0, 0, 0, 2, 2, 1, 1, 1, 2]) == (2 * 4) \
-                + (2 * 5) + (1 * 6) + (1 * 7) + (1 * 8) + (2 * 9)
+               + (2 * 5) + (1 * 6) + (1 * 7) + (1 * 8) + (2 * 9)
 
+    def test_calculate_checksum_with_empty_spaces(self):
+        assert calculate_checksum([0, 4, 4, None, 1, 3, 3, 2, 2]) == (1 * 4) \
+               + (2 * 4) + (4 * 1) + (5 * 3) + (6 * 3) + (7 * 2) + (8 * 2)
 
 
 def test_part_one_given_case():
     raw_input = "2333133121414131402"
     assert part_one(raw_input) == 1928
+
+
+def test_part_two_given_case():
+    raw_input = "2333133121414131402"
+    assert part_two(raw_input) == 2858
