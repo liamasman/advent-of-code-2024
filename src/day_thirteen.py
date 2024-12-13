7beb888893e44bdd42136fd990e5399527cab450
Liam Asman <liam@liamasman.com>
2024-12-13 13:18:03 +0000

    Day 13 - Part One

---
+++ src/day_thirteen.py
@@ -0,0 +1,53 @@
+import re
+
+Coordinates = tuple[int, int]
+
+def part_one(raw_input: str) -> int:
+    inputs = parse_input(raw_input)
+    return sum([price_inputs(calculate_required_inputs(*input)) for input in inputs])
+
+def parse_input(raw_input: str) -> list[tuple[Coordinates, Coordinates, Coordinates]]:
+    matcher = re.compile(r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)")
+    sections = raw_input.strip().split("\n\n")
+    results = []
+    for section in sections:
+        matches = matcher.match(section)
+        results.append(((int(matches.group(1)), int(matches.group(2))),
+                        (int(matches.group(3)), int(matches.group(4))),
+                        (int(matches.group(5)), int(matches.group(6)))))
+    return results
+
+def calculate_required_inputs(button_a: Coordinates, button_b: Coordinates, prize: Coordinates) -> tuple[int, int] | None:
+    x_equation = (button_a[0], button_b[0], prize[0])
+    y_equation = (button_a[1], button_b[1], prize[1])
+    scaled_x_equation = (x_equation[0] * y_equation[0],
+                         x_equation[1] * y_equation[0],
+                         x_equation[2] * y_equation[0])
+    scaled_y_equation = (y_equation[0] * x_equation[0],
+                         y_equation[1] * x_equation[0],
+                         y_equation[2] * x_equation[0])
+    difference = (scaled_x_equation[1] - scaled_y_equation[1],
+                  scaled_x_equation[2] - scaled_y_equation[2])
+    b, r = divmod(difference[1], difference[0])
+    if r != 0:
+        return None
+    if x_equation[1] == 0:
+        a, r = divmod((y_equation[2] - y_equation[1] * b), y_equation[0])
+    else:
+        a,r  = divmod((x_equation[2] - x_equation[1] * b), x_equation[0])
+    if r == 0 and a >= 0 and b >= 0:
+        return a, b
+    return None
+
+def price_inputs(button_presses: tuple[int, int]) -> int:
+    if button_presses is None:
+        return 0
+    return 3 *  button_presses[0] + button_presses[1]
+
+def main():
+    with open("../resources/Day13Input.txt") as f:
+        raw_input = f.read()
+    print(f"Part one: {part_one(raw_input)}")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day_thirteen.py
@@ -0,0 +1,68 @@
+from src.day_thirteen import part_one, parse_input, calculate_required_inputs, price_inputs
+
+
+class TestPartOne:
+    def test_given_example(self):
+        raw_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
+        assert part_one(raw_input) == 480
+
+
+class TestParser:
+    def test_parser(self):
+        raw_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
+        expected = [((94, 34), (22, 67), (8400, 5400)),
+            ((26, 66), (67, 21), (12748, 12176)),
+            ((17, 86), (84, 37), (7870, 6450)),
+            ((69, 23), (27, 71), (18641, 10279))]
+        assert parse_input(raw_input) == expected
+
+
+class TestCalculateRequiredInputs:
+    def test_solveable_inputs_one(self):
+        configuration = (94, 34), (22, 67), (8400, 5400)
+        expected = (80, 40)
+        assert calculate_required_inputs(*configuration) == expected
+
+    def test_solveable_inputs_two(self):
+        configuration = ((17, 86), (84, 37), (7870, 6450))
+        expected = (38, 86)
+        assert calculate_required_inputs(*configuration) == expected
+
+    def test_unsolvable_inputs_one(self):
+        assert calculate_required_inputs((26, 66), (67, 21), (12748, 12176)) is None
+
+    def test_unsolvable_inputs_two(self):
+        assert calculate_required_inputs((69, 23), (27, 71), (18641, 10279)) is None
+
+
+class TestPriceInputs:
+    def test_price_inputs(self):
+        assert price_inputs((80, 40)) == 280
